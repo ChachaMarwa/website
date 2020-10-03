49af89a0eba8f7f90560e1bd2f7cd435dc16c664
chacha <goldcrestmwanza@gmail.com>
2020-10-03 21:01:26 +0300

    tyring out webBluetooth feature

---
+++ script.js
@@ -0,0 +1,8 @@
+navigator.bluetooth.requestDevice({
+    acceptAllDevices: true,
+    optionalServices: ['battery_service']
+  })
+  .then(device => { document.getElementById('heading').innerText = device.name} )
+  .catch(error => { console.log(error); });
+  
+//   There is nothing at the moment &#129299; 
